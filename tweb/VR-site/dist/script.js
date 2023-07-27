898a069b7187a98d7ee901a7684845d66c1a54a6
Alexander <137528976+Derjugin@users.noreply.github.com>
2023-07-27 20:35:29 +0300

    Add files via upload

---
+++ tweb/VR-site/dist/script.js
@@ -0,0 +1,9 @@
+// МЕНЮ
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+  // КОНЕЦ
